c05aad26cbad9fb1e8ac98296341f6a232d11fca
Optaxer | OPR <84898728+OPTAXER@users.noreply.github.com>
2023-01-17 11:56:35 +0530

    opr_dispatch

---
+++ config.lua
@@ -0,0 +1,46 @@
+Config = {}
+Config.Sound = true -- Enable/Disable dispatch sounds
+Config.Framework = 'ESX' -- 'ESX' or 'QBCore'
+Config.Locale = 'en' -- Language
+Config.ShootingAlerts = true -- Enable/Disable Shooting alerts
+Config.ShootingCooldown = 30 -- Seconds
+Config.BlipDeletion = 30 -- Seconds
+
+Config.CommandShow = {
+    command = 'dem',
+    description = 'Open Dispatch'
+}
+
+Config.VehicleRob = {
+    command = 'vehrob',
+    description = 'Vehicle theft'
+}
+
+Config.CommandPanic = {
+    command = 'p',
+    description = 'Emergency button'
+}
+
+Config.CommandClear = {
+    command = 'cleardem',
+    description = 'Clear Alerts'
+}
+
+Config.Jobs = {'police', 'ambulance'}
+
+Config.AllowedJobs = {
+    ["police"] = {
+        name = 'police',
+        label = 'LSPD',
+        command = 'alert',
+        descriptcommand = 'Send an alert to LSPD',
+        panic = true
+    },
+    ["ambulance"] = {
+        name = 'ambulance',
+        label = 'EMS',
+        command = 'alertems',
+        descriptcommand = 'Send an alert to EMS',
+        panic = true
+    }
+}
